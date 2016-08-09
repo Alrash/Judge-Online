cb9fc221cef53cc913cbb1f742b686ebc4ef1289
alrash <kasukuikawai@gmail.com>
2016-08-09 14:47:21 +0800

    add coding changes

--- Generator/windows下替换换行符.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-::Ê¹ÓÃutf-8±àÂë
-chcp 65001
-for /r . %%n in (*.cpp,*.h) do @(
-    for /f "delims=" %%i in (%%n) do @(echo. %%i) >> %%n_tmp
-    del %%n
-)
-ren *.cpp_tmp *.cpp
-ren *.h_tmp *.h
